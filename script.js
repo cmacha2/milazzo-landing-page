af8d5f32e990d7672d4993a0d29388ba3e3d2654
cmacha2 <cmacha2.08@gmail.com>
2024-11-09 15:11:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.button').addEventListener('click', function () {
+    console.log('Botón de WhatsApp clicado');
+    // Aquí puedes agregar integraciones de análisis, como Facebook Pixel
+});
